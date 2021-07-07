86f73b5c0a143858638b3793a8ae222f774d7efc
Stephen Nwaukwa <interprimos80@hotmail.com>
2021-07-06 20:39:11 -0500

    Create board structure

---
+++ main.py
@@ -0,0 +1,16 @@
+def board():
+    """Board used to play the game. Numbers correspond to the spaces"""
+    structure = "    |     |    \n  1 |  2  |  3 \n----|-----|----\n  4 |  5  |  6 \n    |     |    \n" \
+                "----|-----|----\n  7 |  8  |  9 \n    |     |    "
+    return structure
+
+a = board()
+print(a)
+
+
+
+
+
+
+
+
