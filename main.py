e6617e542b914ec13c2b3448eb4b358ed896a3f7
Stephen Nwaukwa <interprimos80@hotmail.com>
2021-07-06 20:42:58 -0500

    Create board structure

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ def board():
     structure = "    |     |    \n  1 |  2  |  3 \n----|-----|----\n  4 |  5  |  6 \n    |     |    \n" \
                 "----|-----|----\n  7 |  8  |  9 \n    |     |    "
     return structure
-
 a = board()
 print(a)
 
